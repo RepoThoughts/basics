8a58e0d215d60cc1c36be1ecce6c11e214228052
Katlego Mogokonyane <katlegovane@gmail.com>
2016-08-16 12:14:06 +0200

    modified few files

--- basics/basics/urls.py
+++ basics/basics/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$',include('crud.urls')),
-    url(r'^crud/',include('crud.urls')),
+    url(r'^$', include('crud.urls')),
+    url(r'^crud/', include('crud.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- basics/crud/models.py
+++ basics/crud/models.py
@@ -10,12 +10,17 @@ class Purchases(models.Model):
     name = models.CharField(max_length=50)
     sold = models.IntegerField()
     price = models.DecimalField(max_length=7, decimal_places=2, max_digits=10)
+    description = models.CharField(max_length=25)
+    state = models.CharField(max_length=25)
 
     def __str__(self):
-        return str(self.item_unique) + "," + self.name + "," + str(self.sold) + "," + str(self.price)
+        return str(self.item_unique) + "," + self.name + "," + str(self.sold) + "," + str(self.price) \
+               + "," + self.description + "," + self.state
 
 
 class PurchasesForms(forms.Form):
     name = forms.CharField(max_length=50)
     sold = forms.IntegerField()
     price = forms.DecimalField(max_digits=10, decimal_places=2)
+    description = forms.CharField(max_length=25)
+    state = forms.CharField(max_length=25)
